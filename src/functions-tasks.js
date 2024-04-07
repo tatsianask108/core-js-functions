098679161c231127bbd4b374d6523072b13537ea
tatsianask108 <tt.laurie@gmail.com>
2024-04-07 13:54:18 +0300

    feat: pass 10

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -134,8 +134,14 @@ function getPolynom(...args) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let cachedRes;
+  return () => {
+    if (!cachedRes) {
+      cachedRes = func();
+    }
+    return cachedRes;
+  };
 }
 
 /**
@@ -180,8 +186,16 @@ function retry(/* func, attempts */) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    const argsString = args.map((arg) => JSON.stringify(arg)).join();
+
+    logFunc(`${func.name}(${argsString}) starts`);
+    const res = func(...args);
+    logFunc(`${func.name}(${argsString}) ends`);
+
+    return res;
+  };
 }
 
 /**
