f91b5ef297e3a160602ca9782b25db689d26ee52
tatsianask108 <tt.laurie@gmail.com>
2024-04-07 14:29:45 +0300

    feat: complete task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -159,8 +159,19 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return () => {
+    let res;
+    for (let i = 0; i < attempts; i += 1) {
+      try {
+        res = func();
+        return res;
+      } catch {
+        //
+      }
+    }
+    return res;
+  };
 }
 
 /**
